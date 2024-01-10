68a674b4471c3ba3adefc073ebe68bb21bf08a0a
Jorrit de Jong <jorrit@jdjict.nl>
2024-01-10 14:20:44 +0100

    bottom plus and transparent appbr

--- huisgenot/lib/src/controller/firebase_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:huisgenot/src/model/feed_model.dart';
-
-Future<void> addFeedItem(FeedItem feedItem) async {
-  final CollectionReference feedCollection =
-      FirebaseFirestore.instance.collection('feed');
-
-  await feedCollection.add(feedItem.toMap());
-}
